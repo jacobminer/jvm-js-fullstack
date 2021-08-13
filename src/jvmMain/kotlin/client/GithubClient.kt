4c77de1d16d95ac1cadf8c3c47681cecef4e6ce5
Jacob Miner <jake@steamclock.com>
2021-08-13 11:39:34 -0700

    Add github client

--- src/jvmMain/kotlin/client/GithubClient.kt
+++ src/jvmMain/kotlin/client/GithubClient.kt
@@ -7,22 +7,41 @@ import io.ktor.client.features.json.serializer.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import logging.log
 
 object GithubClient {
-    private val client = HttpClient(CIO) {
-        install(JsonFeature) {
-            serializer = KotlinxSerializer()
+    private lateinit var _client: HttpClient
+    fun getClient(): HttpClient {
+        if (!this::_client.isInitialized) {
+            _client = HttpClient(CIO) {
+                install(JsonFeature) { serializer = KotlinxSerializer() }
+            }
         }
+        return _client
     }
+
+    fun getUserAgent() = "steamclockkmp-bot"
+    fun getToken() = "token ${System.getenv("crossplat")}"
 
     suspend fun <T: Any> postToUrl(url: String, content: T): HttpResponse {
         println("postToUrl: Calling POST to $url");
-        return client.post(url) {
+        return getClient().post(url) {
             contentType(ContentType.Application.Json)
             body = content
             headers {
-                append("User-Agent", "steamclockkmp-bot")
-                append("Authorization", "token ${System.getenv("crossplat")}")
+                append("User-Agent", getUserAgent())
+                append("Authorization", getToken())
+            }
+        }
+    }
+
+    suspend inline fun <reified T: Any> getFromUrl(issueUrl: String): T {
+        log.debug("getFromUrl", "Calling GET for url = $issueUrl");
+        return getClient().get(issueUrl) {
+            contentType(ContentType.Application.Json)
+            headers {
+                append("User-Agent", getUserAgent())
+                append("Authorization", getToken())
             }
         }
     }
